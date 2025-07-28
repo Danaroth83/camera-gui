40dd6e7d8b1025ef38c06517ed50ff9f42696a90
Daniele Picone <danaroth83@gmail.com>
2025-07-28 23:05:30 +0900

    Adding exposure time dynamic calibration

--- ximea_visualizer/visualizer.py
+++ ximea_visualizer/visualizer.py
@@ -3,7 +3,7 @@ from functools import partial
 from typing import Any
 from dataclasses import dataclass
 from datetime import datetime
-
+import argparse
 
 from ximea import xiapi
 import numpy as np
@@ -14,9 +14,13 @@ from matplotlib.animation import FuncAnimation
 @dataclass
 class CameraState:
     save_folder: Path
+    current_exposure: int
     paused: bool = False
     demosaic: bool = False
     record: bool = False
+    estimating_exposure: bool = False
+    min_exposure: int = 100
+    max_exposure: int = 1_000_000
     save_subfolder: str | None = None
 
     @property
@@ -48,6 +52,29 @@ def get_images(
     tiles = demosaic_tiled(arr=dem)
     return frame_normalized, tiles
 
+def find_exposure_for_saturation(
+    state: CameraState,
+    frame: np.ndarray,
+    max_saturation: int = 8000,
+    tol: int = 100,  # tolerated difference in number of saturated pixels
+) -> tuple[int, bool]:
+    """
+    Binary search for exposure time to keep saturated pixels under max_saturation.
+    """
+
+    converged = False
+    saturated = (frame >= 255).sum()
+    best_exposure = state.current_exposure
+    mid_exposure = (state.max_exposure - state.min_exposure) // 2
+    if saturated > max_saturation:
+        state.max_exposure = state.current_exposure - 1
+    else:
+        best_exposure = mid_exposure
+        state.min_exposure = state.current_exposure + 1
+    if abs(saturated - max_saturation) < tol:
+        converged = True
+    return best_exposure, converged
+
 
 def update(
     frame_index: int,
@@ -56,9 +83,14 @@ def update(
     img: xiapi.Image,
     im: Any, 
 ):
+    if state.estimating_exposure:
+        cam.set_exposure(state.current_exposure)
     cam.get_image(img)
     frame = img.get_image_data_numpy()
-    frame_normalized, demosaiced = get_images(frame=frame, demosaic_flag=state.demosaic)
+    frame_normalized, demosaiced = get_images(
+        frame=frame,
+        demosaic_flag=state.demosaic,
+    )
     if not state.paused:
         if not state.demosaic:
             im.set_data(frame_normalized)
@@ -67,6 +99,11 @@ def update(
     if state.record and state.save_path is not None:
         savefile = state.save_path / f"frame_{frame_index}.npy"
         np.save(file=f"{savefile}", arr=frame)
+    if state.estimating_exposure:
+        state.current_exposure, state.estimating_exposure = find_exposure_for_saturation(
+            state=state,
+            frame=frame,
+        )
     return [im]
 
 
@@ -89,13 +126,17 @@ def on_key(event, state: CameraState):
             print("Recording")
         else:
             print("Stopped recording")
+    if event.key == "e":
+        state.min_exposure = 100
+        state.max_exposure = 1_000_000
+        state.current_exposure = (state.max_exposure - state.min_exposure) // 2
+        state.estimating_exposure = True
 
-
-def main():
+def main_run(exposure: int = 10_000):
     # Initialize camera
     cam = xiapi.Camera()
     cam.open_device()
-    cam.set_exposure(10000)
+    cam.set_exposure(exposure)
     cam.start_acquisition()
     img  = xiapi.Image()
 
@@ -105,13 +146,15 @@ def main():
     cam.get_image(img)
     frame = img.get_image_data_numpy()
     frame_normalized, _ = get_images(frame=frame, demosaic_flag=False)
-    ax.set_title("P to pause/unpause, R to start/stop recording, M to switch view")
+    ax.set_title(
+        "P to pause/unpause, R to start/stop recording, M to switch view, E for calibrating exposure time."
+    )
 
     im = ax.imshow(frame_normalized, cmap="gray")
     
     data_folder = Path(__file__).resolve().parents[1] / "data"
     data_folder.mkdir(parents=False, exist_ok=True)
-    state = CameraState(save_folder=data_folder)
+    state = CameraState(save_folder=data_folder, current_exposure=exposure)
 
     update_fn = partial(update, cam=cam, img=img, im=im, state=state)
     ani = FuncAnimation(fig, update_fn, interval=30, blit=True)
@@ -127,6 +170,18 @@ def main():
         cam.stop_acquisition()
         cam.close_device()
 
+
+def main():
+    parser = argparse.ArgumentParser(description="Setting up exposure time")
+    parser.add_argument(
+        "-e",
+        "--exposure",
+        type=int,
+        default=10000,
+        help="Choose the exposure time in milliseconds.",
+    )
+    args = parser.parse_args()
+    main_run(exposure=args.exposure)
 
 if __name__ == "__main__":
     main()
