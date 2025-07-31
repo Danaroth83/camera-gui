9dad9190f80dbc008e5c34d6201ef837ce5faa69
Daniele Picone <danaroth83@gmail.com>
2025-07-31 22:04:16 +0900

    TIS camera

--- ximea_visualizer/tis_interface.py
+++ ximea_visualizer/tis_interface.py
@@ -1,8 +1,9 @@
+from dataclasses import dataclass
+from pathlib import Path
 
-
+import imagingcontrol4 as ic4
 from ximea_visualizer.mock_interface import Camera
 
-<<<<<<< HEAD
 
 @dataclass
 class TisCameraState:
@@ -16,30 +17,42 @@ class TisCameraState:
     # save_subfolder: str | None = None
 
     def sync(self, cam: ic4.Grabber):
+        pass
 
 
-
-        self.current_exposure = cam.get_exposure()
-        if cam.get_image_data_bit_depth() == "XI_BPP_10":
-            self.bit_depth_10bits = True
-        else:
-            self.bit_depth_10bits = False
-
-    @property
-    def save_path(self) -> Path | None:
-        if self.save_subfolder is None:
-            return None
-        else:
-            return self.save_folder / self.save_subfolder
-
-    @property
-    def dynamic_range(self):
-        return XIMEA_DYN_RANGE_10BIT if self.bit_depth_10bits else XIMEA_DYN_RANGE_8BIT
-
-
-=======
->>>>>>> 739332a8026cf4380f14b4881ecbf033cd842a6e
 class TisCamera(Camera):
+    grabber : ic4.Grabber
+    sink: ic4.SnapSink | None
+    state: TisCameraState
 
-    def open():
+    def __init__(self): 
+        self.grabber = ic4.Grabber()
+        self.sink = None
+        data_path = Path(__file__).resolve().parents[1] / "data"
+        data_path.mkdir(parents=False, exist_ok=True)
+        data_path = data_path / "tis"
+        data_path.mkdir(parents=False, exist_ok=True)
+        state = TisCameraState(
+            save_folder=data_path,
+            current_exposure=10_000,
+        )
+        self.state = state
+
+    def open(self):
+        first_device_info = ic4.DeviceEnum.devices()[0]
+        self.grabber.device_open(first_device_info)
+        self.sink = ic4.SnapSink()
+        self.grabber.stream_setup(self.sink, setup_option=ic4.StreamSetupOption.ACQUISITION_START)
         
+    def close(self):
+        self.grabber.stream_stop()
+        
+    def set_save_subfolder(self, subfolder: str) -> None:
+        self.state.save_subfolder = subfolder
+        self.state.save_path.mkdir(parents=False, exist_ok=True)
+
+    def save_folder(self) -> Path:
+        return self.state.save_path
+    
+    def exception_type(self) -> Exception:
+        return ic4.Exception
