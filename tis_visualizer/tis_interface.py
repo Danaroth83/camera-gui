36fa7a7083882b133d7610b3bcf174f98299ccf1
Mohamad Jouni <mohamad.jouni@grenoble-inp.fr>
2025-07-31 22:21:45 +0900

    Added TIS MWE

--- tis_visualizer/tis_interface.py
+++ tis_visualizer/tis_interface.py
@@ -1,29 +1,58 @@
-import imagingcontrol4 as ic4
-from ximea_visualizer.mock_interface import Camera
+from dataclasses import dataclass
+from pathlib import Path
 
+import imagingcontrol4 as ic4
+
+from ximea_visualizer.mock_interface import Camera
 
 TIS_HEIGHT = 1920
 TIS_WIDTH = 1200
 
 
+@dataclass
+class TisCameraState:
+    save_folder: Path
+    current_exposure: int
+    save_subfolder: str | None = None
+
+    @property
+    def save_path(self) -> Path:
+        if self.save_subfolder is None:
+            return self.save_folder
+        else:
+            return self.save_folder / self.save_subfolder
+
+
 class TisCamera(Camera):
-    def __init__(
-            self,
-            device_idx: int = 0,
-    ):
-        self.camera = ic4.DeviceEnum.devices()[device_idx]
+    grabber: ic4.Grabber
+    sink: ic4.SnapSink | None
+    state: TisCameraState
+
+    def __init__(self):
+        self.grabber = ic4.Grabber()
+        self.sink = None
+        data_path = Path(__file__).resolve().parents[1] / "data"
+        data_path.mkdir(parents=False, exist_ok=True)
+        data_path = data_path / "tis"
+        data_path.mkdir(parents=False, exist_ok=True)
+        state = TisCameraState(
+            save_folder=data_path,
+            current_exposure=10_000,
+        )
+        self.state = state
 
     def open(self):
-        # Create a Grabber object
-        grabber = ic4.Grabber()
-        # Open the first available video capture device
-        device_info =
-        grabber.device_open(first_device_info)
+        first_device_info = ic4.DeviceEnum.devices()[0]
+        self.grabber.device_open(first_device_info)
+        self.sink = ic4.SnapSink()
+        self.grabber.stream_setup(self.sink, setup_option=ic4.StreamSetupOption.ACQUISITION_START)
 
+    def close(self):
+        self.grabber.stream_stop()
 
-def main():
-    pass
+    def set_save_subfolder(self, subfolder: str) -> None:
+        self.state.save_subfolder = subfolder
+        self.state.save_path.mkdir(parents=False, exist_ok=True)
 
-
-if __name__ == "__main__":
-    main()
+    def save_folder(self) -> Path:
+        return self.state.save_path
