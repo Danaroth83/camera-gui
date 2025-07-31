0be6bc71f0e33447db345c37e835d2fc9877ba1f
Daniele Picone <danaroth83@gmail.com>
2025-08-01 01:12:14 +0900

    Minor change

--- the_imaging_source_visualizer/step04_grab_image.py
+++ the_imaging_source_visualizer/step04_grab_image.py
@@ -14,8 +14,8 @@ def main():
     # grabber.device_property_map.set_value(ic4.PropId.PIXEL_FORMAT, ic4.PixelFormat.BGR8)
     #
     # # Set the resolution to 640x480
-    # grabber.device_property_map.set_value(ic4.PropId.WIDTH, 640)
-    # grabber.device_property_map.set_value(ic4.PropId.HEIGHT, 480)
+    grabber.device_property_map.set_value(ic4.PropId.WIDTH, 640)
+    grabber.device_property_map.set_value(ic4.PropId.HEIGHT, 480)
 
     # Create a SnapSink. A SnapSink allows grabbing single images (or image sequences) out of a data stream.
     sink = ic4.SnapSink()
